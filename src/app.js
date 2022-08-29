406740ad070efc862fbfd9f3bb5e94ab839091fc
양성훈 <tjdgns7533@gmail.com>
2022-08-29 21:57:13 +0900

    클래스 컴포넌트

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 /* @jsx createElement */
-import { createDOM, createElement, render } from './react';
+import { createElement, render, Component } from './react';
 
-const vdom = <p>
-  <h1>React 만들기</h1>
+class Title extends Component {
+  render() {
+    return <h1>{ this.props.children }</h1>;
+  }
+}
+
+function Item(props) {
+  return <li style={`color:${props.color}`}>{props.children}</li>
+}
+
+const App = () => <p>
+  <Title label="React">React 정말 클래스 컴포넌트 잘 만들기</Title>
   <ul>
-    <li style="color:red">첫 번째 아이템</li>
-    <li style="color:blue">두 번째 아이템</li>
-    <li style="color:green">세 번째 아이템</li>
+    <Item color="red">첫 번째 아이템</Item>
+    <Item color="green">두 번째 아이템</Item>
+    <Item color="blue">세 번째 아이템</Item>
   </ul>
 </p>
 
-render(vdom, document.querySelector('#root'));
+render(<App />, document.querySelector('#root'));
--- src/react.js
+++ src/react.js
@@ -1,3 +1,10 @@
+
+export class Component {
+  constructor(props) {
+    this.props = props;
+  }
+}
+
 export function createDOM(node) {
   if (typeof node === 'string') {
     return document.createTextNode(node);
@@ -6,7 +13,7 @@ export function createDOM(node) {
   const element = document.createElement(node.tag);
 
   Object.entries(node.props)
-    .forEach(([name, value]) => element.setAttribute(name, value)); 
+    .forEach(([name, value]) => element.setAttribute(name, value));
 
   node.children
     .map(createDOM)
@@ -15,11 +22,33 @@ export function createDOM(node) {
   return element;
 }
 
+function makeProps(props, children) {
+  return {
+    ...props,
+    children: children.length === 1 ? children[0] : children,
+  };
+}
+
 export function createElement(tag, props, ...children) {
   props = props || {};
-  return { tag, props, children };
+  
+  if (typeof tag === 'function') {
+    if (tag.prototype instanceof Component) {
+      const instance = new tag(makeProps(props, children));
+      return instance.render();
+    } else {
+      if (children.length > 0) {
+        return tag(makeProps(props, children))
+      } else {
+        return tag(props);
+      }
+    }
+
+  } else {
+    return { tag, props, children };
+  }
 }
 
 export function render(vdom, container) {
-  container.appendChild(createDOM(vdom));
+  container.appendChild(createDOM(vdom));  
 }
